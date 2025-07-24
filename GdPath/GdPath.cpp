53a94320fa3dc42920a9833ea956f700b7ca4950
Alexander Bondarenco <sasha.199568@gmail.com>
2025-07-24 19:19:28 +0300

    Temperature conversion

--- GdPath/GdPath.cpp
+++ GdPath/GdPath.cpp
@@ -8,5 +8,35 @@ int main()
 
     printf("Hello, World\n");
 
+    float fahr;
+    float celc;
+    int lower = 0;
+    int upper = 300;
+    int step = 20;
+
+    printf("Fahrenheit to Celcius\n");
+    fahr = lower;
+    while (fahr <= upper)
+    {
+        celc = 5 * (fahr - 32) / 9;
+        // celc / 5 = (fahr - 32) / 9
+        // celc / 5 * 9 = fahr - 32
+        // celc / 5 * 9 + 32 = fahr
+        printf("%3.0f %6.1f\n", fahr, celc);
+        fahr += step;
+    }
+
+    printf("Celcius to Fahrenheit\n");
+    lower = -40;
+    upper = 120;
+    step = 10;
+    celc = lower;
+    while (celc <= upper)
+    {
+        fahr = celc / 5 * 9 + 32;
+        printf("%3.0f %6.1f\n", celc, fahr);
+        celc += step;
+    }
+
     return 0;
 }
